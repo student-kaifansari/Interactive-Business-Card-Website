85e819bc41ad7aa2c05b8620721ff381091f3ed1
student-kaifansari <ansarikaif51692@gmail.com>
2025-04-04 16:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.navbar-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
